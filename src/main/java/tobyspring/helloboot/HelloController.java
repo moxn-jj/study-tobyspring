0a27ed7ec87a14b126c8b3332f5732dd349460d5
mun <mun.xml@gmail.com>
2023-12-24 00:01:01 +0900

    =서블릿 컨테이너 만들어 보기

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -6,6 +6,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    /**
+     * 동작되는 내용 :
+     * /hello라는 경로로 들어오는 요청을 받아서
+     * 파라미터 name 값을 가져와서 hello 뒤에 이름을 붙여 리턴함.
+     * RestContoller로 등록해두었기 때문에 웹 응답이 만들어질 때
+     * 이 method의 스트링을 보고 Content-type이 자동으로 결정됨.
+     * 여기서 리턴 타입이 String이기 때문에 text/plain; 타입의 Content-type이 만들어지고
+     * 리턴된 문자열이 그대로 응답 body에 들어감.
+     * @param name
+     * @return
+     */
     @GetMapping("/hello")
     public String hello(String name){
         return "Hello " + name;
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,31 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
 
-@SpringBootApplication
+// 0. 이 코드 없이 스프링 부트랑 똑같이 동작 시켜보자
+//@SpringBootApplication
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HellobootApplication.class, args);
+
+        // 0. 이 코드 없이 스프링 부트랑 똑같이 동작 시켜보자
+        // SpringApplication.run(HellobootApplication.class, args);
+
+        // 1. 메인 메소드가 진짜 동작하는게 맞나? 확인해보자.
+        System.out.println("Hello Containerless Standalone Application");
+
+        // 2. 서블릿 컨테이너 만들기
+        // => Embedded Tomcat. 즉, 내장형 톰캣 사용해보자
+        // new Tomcat().start(); // 이건 완전 생짜 Tomcat. 이 코드로 톰캣 동작 시키려면 설정이 많이~ 필요함
+        // 그래서 Spring Boot가 미리 설정 다해둔 도우미 클래스가 바로 이것!
+        TomcatServletWebServerFactory serverFactory =  new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();// servlet container를 만드는 생성 함수
+        webServer.start(); // Tomcat Servlet Container가 동작!
+        // http -v :8080 요청하면 404 error 리턴함 => 즉, 톰캣이 진짜 떠 있다!
+
+
+
     }
 
 }
